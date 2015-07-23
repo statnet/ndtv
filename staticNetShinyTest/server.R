616e6f2aef5b6f6b7b0a4e498189c44b2154ba32
skyebend <skyebend@9cb281f4-553f-406f-8d7f-11f072bbfe46>
2015-07-23 22:08:42 +0000

    added example shiny app that uses ndtv-d3 render an interactive static network

---
+++ staticNetShinyTest/server.R
@@ -0,0 +1,42 @@
+library(shiny)
+library(ndtv)
+library(network)
+data(emon)  # load datasets to use
+
+# Define server logic required to draw a interactive plot
+shinyServer(function(input, output) {
+  
+  
+  output$netPlot <- renderUI({
+    # load the network
+    net<-emon[[as.numeric(input$networkSelection)]]
+    netName<-names(emon)[as.numeric(input$networkSelection)]
+    # compute some stuff to be used in display
+    totalStaff<-net%v%'Volunteer.Staff'+emon[[5]]%v%'Paid.Staff'
+    sizeScale<-ifelse(is.na(totalStaff),1,totalStaff/100+1)
+
+    # call the d3Movie render on a static network
+    # capturing its output and passing it to the shiny app as HTML
+    outHTML<-capture.output(render.d3movie(net, 
+                 main=paste("Search & Rescue org communication network for",netName),                      
+                 #format HTML text for edge tool tips                           
+                 vertex.tooltip=paste("<strong>",net%v%'vertex.names',"</strong><br>",
+                                      "Decision Rank Score:",net%v%'Decision.Rank.Score',"<br>",
+                                      "Command Rank Score:",net%v%'Command.Rank.Score',"<br>",
+                                      "Formalization:",net%v%'Formalization',"<br>",
+                                      "Location:",net%v%'Location',"<br>",
+                                      "Sponsorship:",net%v%'Sponsorship',"<br>"),
+                 vertex.cex=sizeScale,
+                vertex.col=grDevices::adjustcolor(as.color(net%v%'Sponsorship'),alpha.f=0.5),
+                # format HTML text for edge tool tips
+                edge.tooltip=paste('Frequency:',net%e%'Frequency'),
+                edge.lwd='Frequency',
+                edge.col='#00000055',
+                output.mode = 'inline',  # output directly instead of to file
+                script.type='remoteSrc', # link to .js files instead of including them directly
+                 launchBrowser = FALSE  # don't load in a web browser
+                ))
+    
+    return(HTML(outHTML))
+  })
+})
---
+++ staticNetShinyTest/ui.R
@@ -0,0 +1,23 @@
+library(shiny)
+
+# Define UI for application that renders static ndtv-d3 view of networks
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("interactive network with ndtv test"),
+  
+  # Sidebar with a selector for picking which network to render
+  sidebarLayout(
+    sidebarPanel(
+      
+    selectInput('networkSelection', 'Select "emon" example network:', 1:7)
+    ),
+    
+    # Show the interactive network plot
+    mainPanel(
+      h2('ndtv-d3 interactive network plot'),
+      p('Only works when shiny app is displayed in a web browser. Mouse wheel zooms, click for tooltips, drag to pan, double click to highlight neighbors.'),
+      htmlOutput("netPlot")
+    )
+  )
+))
