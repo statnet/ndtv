2f2ff7338e0a9d51811a8044ffafb437fc64a4fe
skyebend <skyebend@9cb281f4-553f-406f-8d7f-11f072bbfe46>
2015-09-30 00:29:17 +0000

    added a 'diffusionTimeline' to plot transmssion tree depth against infection time.  maybe this should go in tsna?  will probably be called by epimodel.

---
+++ ndtv/R/diffusionTimeline.R
@@ -0,0 +1,114 @@
+
+
+
+diffusionTimeline<-function(x,time.attr,
+                          label,
+                          displaylabels = !missing(label),
+                          label.cex = 0.7,
+                          label.col = 1,
+                          vertex.col = 2,
+                          vertex.border=1,
+                          vertex.lwd=1,
+                          vertex.sides = 50,
+                          vertex.cex = 1,
+                          edge.col = 'gray',
+                          edge.lty = 1,
+                          edge.lwd = 1,
+                          xlab='time',
+                          ylab='generation',
+                          ...){
+  net<-as.network(x)
+  if(missing(time.attr)){
+    if('transmat'%in%class(x)){
+      time.attr<-'at'
+    } else if ('tPath'%in%class(x)){
+      time.attr<-'tdist'
+    } else {
+      stop('time.attr argument must be given')
+    }
+  }
+  if (!time.attr%in%list.vertex.attributes(net)){
+    stop("time.attr ",time.attr,' is not a vertex attribute of the network')
+  }
+  
+  el<-as.edgelist(net)
+  times<-get.vertex.attribute(net,time.attr)
+  coords<-matrix(0,nrow=network.size(net),ncol=2)
+  yBin<-0
+  if(is.tPath(x)){
+    # assume tPath for now
+    yBin<-net%v%'gsteps'
+    coords<-cbind(times,yBin)
+  } else {
+    # find roots
+    v<-setdiff(unique(el[,1]),unique(el[,2]))
+    
+    visited<-integer(0)
+    while(length(v)>0){
+      visited<-c(visited,v[1])
+      coords[v[1],]<-c(times[v[1]],yBin)
+      kids<-el[el[,1]==v[1],2] # look up kids on the edgelist
+      # in case network was not actually a tree, make sure we don't loop forever
+      if(any(kids%in%visited)){
+        stop('vertex was revisited: network does not appear to be a tree')
+      }
+      v<-c(v[-1],kids)
+      yBin<-yBin+1
+    }
+  }
+  #op <- par(no.readonly = TRUE)
+  
+  # set up the plotting window
+  plot(coords,pch=NA,xlab=xlab,ylab=ylab,...)
+  
+  # expand the various plot parameters using network defaults
+  
+  vertex.col<- plotArgs.network(net,'vertex.col',vertex.col)
+  vertex.border<-plotArgs.network(net,'vertex.border',vertex.border)
+  vertex.lwd<-plotArgs.network(net,'vertex.lwd',vertex.lwd)
+  vertex.sides<-plotArgs.network(net,'vertex.sides',vertex.sides)
+  vertex.cex<-plotArgs.network(net,'vertex.cex',vertex.cex)
+  # remap vertex.sides to a vertex pch approximation
+  vertex.pch <- sapply(vertex.sides,function(sides){
+    switch (as.character(sides),
+            '3' = 24,
+            '4' = 22,
+            '50' = 21)})
+  
+  edge.col<-plotArgs.network(net,'edge.col',edge.col)
+  edge.lty <- plotArgs.network(net,'edge.lty',edge.lty)
+  edge.lwd <- plotArgs.network(net,'edge.lwd',edge.lwd)
+  if(missing(label)){
+    label<-network.vertex.names(net)
+  }
+  label <-plotArgs.network(net,'labels',label)
+  label.col<-plotArgs.network(net,'label.col',label.col)
+  label.pos<-4
+  label.cex<-plotArgs.network(net,'label.cex',label.cex)
+  
+  # plot the 'edges'
+  if(nrow(el)>0){
+    lapply(1:nrow(el),function(e){
+      lines(c(coords[el[e,1],1],
+              coords[el[e,2],1]), # xcoords
+            c(coords[el[e,1],2],
+              coords[el[e,2],2]),# ycoords
+            col=edge.col[e],
+            lwd=edge.lwd[e],
+            lty=edge.lty[e]
+      )
+    })
+  }
+  
+  # plot the vertices
+  if(network.size(net)>0){
+    points(coords[,1],coords[,2],bg=vertex.col,pch=vertex.pch,col=vertex.border,lwd=vertex.lwd,cex=vertex.cex)
+    
+    # plot labels
+    if(displaylabels){
+      text(coords[,1],coords[,2],labels = label,col=label.col,pos=label.pos,cex=label.cex)
+    }
+  }
+  #par(op)
+  invisible(coords)
+}
---
+++ ndtv/tests/diffusionTimelineTests.R
@@ -0,0 +1,32 @@
+# tests for diffusion timeline
+require(testthat)
+
+#require(tsna)
+#data(moodyContactSim)
+#v10path<-tPath(moodyContactSim,v=10,start=0)
+#v11path<-tPath(moodyContactSim,v=11,start=0)
+#v12path<-tPath(moodyContactSim,v=12,start=0)
+
+# creating v10 path via structure for now so don't bring in tsna dep
+v10path<-structure(list(tdist = c(583, 494, 634, 40, 712, 701, 224, 719,  674, 0, 749, 621, 453, 665, 709, 575), previous = c(16, 13, 13,  10, 13, 16, 10, 13, 1, 0, 8, 1, 4, 4, 2, 2), gsteps = c(5, 3,  3, 1, 3, 5, 1, 3, 6, 0, 4, 6, 2, 2, 4, 4), start = 0, end = Inf,      direction = "fwd", type = "earliest.arrive"), .Names = c("tdist",  "previous", "gsteps", "start", "end", "direction", "type"), class = c("tPath",  "list"))
+
+diffusionTimeline(v10path)
+diffusionTimeline(v10path,displaylabels = TRUE)
+diffusionTimeline(v10path,displaylabels = TRUE,label='a')
+diffusionTimeline(v10path,displaylabels = TRUE,label.col='blue')
+diffusionTimeline(v10path,displaylabels = TRUE,label.cex=2)
+diffusionTimeline(v10path,vertex.col='blue')
+diffusionTimeline(v10path,vertex.border='blue')
+diffusionTimeline(v10path,vertex.lwd=1:3)
+diffusionTimeline(v10path,vertex.sides = c(3,4,50))
+diffusionTimeline(v10path,edge.col='blue')
+diffusionTimeline(v10path,edge.lwd=5)
+diffusionTimeline(v10path,edge.lty=2)
+diffusionTimeline(v10path,xlab='hello',ylab='world',main='diffusion tree timeline plot')
+
+
+data("toy_epi_sim")
+
+expect_error(diffusionTimeline(toy_epi_sim,time.attr='foo'),regexp = 'is not a vertex attribute of the network')
+# check for appropriate error if input not a tree
+expect_error(diffusionTimeline(toy_epi_sim,time.attr='testatus.active'),regexp = 'network does not appear to be a tree')
