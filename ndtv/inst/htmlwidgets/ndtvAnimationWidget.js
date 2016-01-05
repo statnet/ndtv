1e87d5fb964d1da1da2741359ddc3d73717a3e3b
skyebend <skyebend@9cb281f4-553f-406f-8d7f-11f072bbfe46>
2016-01-05 21:25:33 +0000

    updated ndtv-d3 htmlwidget javascript to use unique id in order to support multiple widgets per rmarkdown page. Fixes #1258

--- ndtv/inst/htmlwidgets/ndtvAnimationWidget.js
+++ ndtv/inst/htmlwidgets/ndtvAnimationWidget.js
@@ -6,31 +6,36 @@ HTMLWidgets.widget({
   
 
   initialize: function(el, width, height) {
-    
-    // check if the div already exists (if this is a re-render)
-    if (!d3.select('#ndtvAnimation').empty()){
+    // get the DOM id of parent widget so can use it to construct a unique id for the animation
+    ndtvId=d3.select(el).attr('id')+'_ndtvAnimation';
+    // check if the ndtvAnimation div already exists as a child(if this is a re-render)
+    if (!d3.select('#'+ndtvId).empty()){
       // remove its contents
-      d3.select('#ndtvAnimation').selectAll("*").remove()
+      d3.select('#'+ndtvId).selectAll("*").remove();
     } else {
       // append the div into the DOM that ndtv_d3 will renderinto
-      d3.select(el).append('div').attr('id','ndtvAnimation').style('width',width+'px').style('height',height+'px');
+      d3.select(el).append('div').attr('id',ndtvId).style('width',width+'px').style('height',height+'px');
     }
     return{};
   },
 
   renderValue: function(el, x, instance) {
+      // get the DOM id of parent widget so can use it to construct a unique id for the animation
+      ndtvId=d3.select(el).attr('id')+'_ndtvAnimation';
       // remove previous contents of the div (re render)
-      d3.select('#ndtvAnimation').selectAll("*").remove()
+      d3.select('#'+ndtvId).selectAll("*").remove();
       // copy the graph options into the options array
       var options = x.animationOptions;
       options.graphData = x.graphData;
-      // create the new ndtv instance (this will load the svg into the div id named)
-      graph = new ndtv_d3(options, '#ndtvAnimation');
+      // create the new ndtv instance (this will load the svg into the div with id ndtvId)
+      graph = new ndtv_d3(options, '#'+ndtvId);
       return(graph);
   },
 
   resize: function(el, width, height, instance) {
-    d3.select('#ndtvAnimation').style('width',width+'px').style('height',height+'px');
+    // get the DOM id of parent widget so can use it to construct a unique id for the animation
+    ndtvId=d3.select(el).attr('id')+'_ndtvAnimation';
+    d3.select('#'+ndtvId).style('width',width+'px').style('height',height+'px');
     graph.resizeGraph();  
 
   }
