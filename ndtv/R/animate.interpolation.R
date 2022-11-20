3ecc78280ab453b0f69a16446999ae2481c7903d
Skye Bender-deMoll <skyebend@skyeome.net>
2022-11-20 11:48:33 -0800

    converting flag urls to https

--- ndtv/R/animate.interpolation.R
+++ ndtv/R/animate.interpolation.R
@@ -1,9 +1,9 @@
 #  File R/animate.interpolation.R in package ndtv, part of the Statnet suite
-#  of packages for network analysis, http://statnet.org .
+#  of packages for network analysis, https://statnet.org .
 #
 #  This software is distributed under the GPL-3 license.  It is free,
 #  open source, and has the attribution requirements (GPL Section 7) at
-#  http://statnet.org/attribution
+#  https://statnet.org/attribution
 #
 #  Copyright 2012-2016 Statnet Commons
 #######################################################################
